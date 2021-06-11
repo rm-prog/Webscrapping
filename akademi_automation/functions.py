5400cb049286be138a7ff4e374b3ff5fe47b663e
Rminollari <rminollari@gmail.com>
2021-06-11 17:51:16 +0200

    Comments added

--- akademi_automation/functions.py
+++ akademi_automation/functions.py
@@ -17,13 +17,16 @@ def open_zoom(first_link, second_link, third_link):
     print(email)
     print(password)
 
+    # Open chrome web browser
     path = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(path)
 
+    # Go to Akademia.al
     driver.get("https://akademi.al")
 
     time.sleep(1)   
     try:
+        # Click login button
         login_button = WebDriverWait(driver, 6).until(
             EC.presence_of_element_located((By.LINK_TEXT, "Hyr"))
         )
@@ -31,6 +34,7 @@ def open_zoom(first_link, second_link, third_link):
 
         time.sleep(1)
 
+        # Find input fields where you put email and password
         email_input = WebDriverWait(driver, 6).until(
             EC.presence_of_element_located((By.ID, "username"))
         )
@@ -38,12 +42,14 @@ def open_zoom(first_link, second_link, third_link):
             EC.presence_of_element_located((By.ID, "password"))
         )   
 
+        # Type in email and password
         email_input.clear()
         password_input.clear()
     
         email_input.send_keys(email)
         password_input.send_keys(password)
 
+        # Click login button
         submit_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "button"))
         )
@@ -53,26 +59,34 @@ def open_zoom(first_link, second_link, third_link):
     except:
         driver.quit()
 
+    # Booleans to check if user has entered the zoom class or not
     enter_first = False
     enter_second = False
     enter_third = False
+    # Check the time 
     while datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour < 13:
         if datetime.datetime.now().hour == 10 and enter_first != True:
+            # Go to the first online class of the day
             driver.get(first_link)
             while True:   
                 try:
+                    # Look if the link to join zoom class exists
                     join_button = WebDriverWait(driver, 10).until(
                         EC.presence_of_element_located((By.LINK_TEXT, "Bëhu pjesë e klasës live"))
                     )
+                    # if so go to zoom class and alarm
                     join_button.click()
                     playsound(audio_file)
+                    # User has entered zoom class
                     enter_first = True
                     
                     break
                 except:
+                    # if link to join zoom class not found, then wait for 1 minute and try again
                     time.sleep(60)
                     driver.refresh()
                     continue
+        # Same as above
         elif datetime.datetime.now().hour == 11 and enter_second != True:
             driver.get(second_link)
             while True:    
@@ -88,6 +102,7 @@ def open_zoom(first_link, second_link, third_link):
                     time.sleep(60)
                     driver.refresh()
                     continue
+        # Same
         elif datetime.datetime.now().hour == 12 and enter_third != True:
             driver.get(third_link)
             while True:   
@@ -103,4 +118,5 @@ def open_zoom(first_link, second_link, third_link):
                     time.sleep(60)
                     driver.refresh()
                     continue
+        # if lessons havent started yet then wait for 2 minutes
         time.sleep(120)
--- akademi_automation/main.py
+++ akademi_automation/main.py
@@ -2,6 +2,8 @@ import time
 import datetime
 from functions import open_zoom
     
+
+# <----------------- Links for online classes ---------------------------->    
 kimi_link = "https://app.akademi.al/course/_student/d316df2a-4bd0-4294-8e43-f94744bacbdb"
 biologji_link = "https://app.akademi.al/course/_student/7178b586-5493-4d1f-bfbb-5c4a3806103d"
 tik_link = "https://app.akademi.al/course/_student/821a6c1f-e18c-4c5c-80ec-43a81a8b352c"
@@ -17,8 +19,11 @@ histori_link = "https://app.akademi.al/course/_student/04db553d-6409-42b7-bcc3-1
 filozofi_link = "https://app.akademi.al/course/_student/44fc3be3-4f87-42e8-8e93-22fff11ef2f4"
 fiskulture_link = "https://app.akademi.al/course/_student/0f831d9f-d3c0-4475-8c2f-840dc7942672"
 
+
 day_of_week = datetime.datetime.today().weekday()
 
+# <---------------------- Check day of the week and the hour ------------------------>
+# <---------------------- Pass the class links corresponding to the day of the week
 def start_lesson():
     if day_of_week == 0:
         if datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour < 13:
@@ -33,13 +38,13 @@ def start_lesson():
         if datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour < 13:
             open_zoom(mat_link, fiskulture_link, fizik_link)            
     if day_of_week == 4:
-        if datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour <= 13:
+        if datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour < 13:
             cift_tek = int(input("Tik apo anglisht (1, 2)"))
             if cift_tek == 1: open_zoom(gjuhe_link, tik_link, frengjisht_link)   
             else: open_zoom(gjuhe_link, anglisht_link, frengjisht_link)         
 
 
 if __name__ == "__main__":
-    while datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour <= 13: 
+    while datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour < 13: 
         start_lesson()
         time.sleep(60)
