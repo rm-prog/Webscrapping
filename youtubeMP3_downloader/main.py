52fc73f599e6fc68281c782c450d5019faf2f310
Rminollari <rminollari@gmail.com>
2021-12-26 11:58:23 +0100

    fixed youtube mp3 converter; changed website used to convert

--- youtubeMP3_downloader/main.py
+++ youtubeMP3_downloader/main.py
@@ -4,43 +4,62 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
 def convertVideoToMP3(url):
 
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
     # Open chrome web browser
     path = "C:\Program Files (x86)\chromedriver.exe"
     driver = webdriver.Chrome(path)
 
     # Go to youtube MP3 converter
-    driver.get("https://320ytmp3.com/en30/")
+    driver.get("https://get.topsandtees.space/")
 
-    time.sleep(2)
+    time.sleep(3)
 
     try:
-        # find video URL input and enter url
-        input_field = WebDriverWait(driver, 6).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "form-control-lg"))
-            )
-        input_field.clear()
+        # find input field and enter video URL
+        input_field = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.TAG_NAME, "input"))
+        )
         input_field.send_keys(url)
 
+        time.sleep(1)
+
+        # find searchBtn and click it
+        search_btn = WebDriverWait(driver, 8).until(
+            EC.presence_of_element_located((By.TAG_NAME, "button"))
+        )
+        search_btn.click()
+
         time.sleep(2)
+
+        # find downloadBtn and click it
+        download_btn = WebDriverWait(driver, 6).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "search-item__download"))
+        )
+        download_btn.click()
+
+        time.sleep(1)
+
+        # click downloadBtn again
+
+        download_btn = WebDriverWait(driver, 6).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "search-item__download"))
+        )
+        download_btn.click()
+
+        time.sleep(10)
     except:
         driver.quit()
 
-    # find and click searchBtn
-    driver.find_element_by_xpath("//button[text()='Search']").click()
-    time.sleep(5)
-    # find and click convertBtn
-    convert_btn = driver.find_element_by_id("cvt-btn")
-    time.sleep(5)
-    convert_btn.click()
-    time.sleep(5)
-
-
+    
 
 if __name__ == "__main__":
     video_url = sys.argv[1]
